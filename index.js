1093959e6efc722e6cc830a829d6416fddec3c57
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-05 17:52:39 -0400

    phase-1-adding-behavior-with-methods/
    phase-1-adding-behavior-with-methods/

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+class Cat {
+    constructor(korat) {
+    this.cat = korat;
+    this.breed = cat;
+  }
+}
+class Dog {
+    constructor(germanshorthaired) {
+    this.dog = german-short-haired;
+    this.dog = medium-sized;
+ }
+
+}
+class Bird {
+    constructor(parrot) {
+        this.bird = rainbow-colored;
+        this.bird = talkingparrot;
+  }
+}
