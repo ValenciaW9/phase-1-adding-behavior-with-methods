1ef8ddef87d144634686c1577445b7aa78593487
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-05 21:06:40 -0400

    phase-1-adding-behavior-with-methods/
    phase-1-adding-behavior-with-methods/

--- phase-1-adding-behavior-with-methods-master 2/index.js
+++ phase-1-adding-behavior-with-methods-master 2/index.js
@@ -31,9 +31,9 @@ class Bird {
 
   speak() {
     if (this.sex === "male") {
-      return `It\'s me, ${this.name}, the parrot!`;
+      return `It's me, ${this.name}, the parrot!`;
     } else {
-      return `${this.name} says squawk!`;
+      return `It's me! ${this.name}, the parrot!`;
     }
   }
 }
