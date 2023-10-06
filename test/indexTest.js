83c77b9a1d8d62c78777c262c374e7666e400936
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-10-05 20:55:39 -0400

    phase-1-adding-behavior-with-methods/
    phase-1-adding-behavior-with-methods/

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,33 +1,44 @@
-describe("Cat", () => {
-  let cat;
+require ( './helpers.js' );
 
+let cat, dog, bird, bird2;
+
+describe("cat", () => {
   beforeEach(() => {
-    cat = new Cat("Korat");
-  });
+    cat = new Cat("Sasha", "female")
+  })
 
-  it("has a breed property", () => {
-    expect(cat.breed).to.equal("Korat");
-  });
-});
-describe("Dog", () => {
-  let dog;
-
+  it("has a class of cat", () => {
+    expect(new Cat()).to.be.an.instanceof(Cat);
+  })
+  it("cat.speak() returns '[name]' says meow!", () => {
+    expect(cat.speak()).to.eq('Sasha says meow!')
+  })
+})
+describe("dog", () => {
   beforeEach(() => {
-    dog = new Dog("German Shorthaired");
-  });
+    dog = new Dog("Rufio", "male")
+  })
 
-  it("has a breed property", () => {
-    expect(dog.breed).to.equal("German Shorthaired");
-  });
-});
-describe("Bird", () => {
-  let bird;
-
+  it("has a class of dog", () => {
+    expect(new Dog()).to.be.an.instanceof(Dog);
+  })
+  it("dog.speak() returns '[name] says woof!'", () => {
+    expect(dog.speak()).to.eq('Rufio says woof!')
+  })
+})
+describe("bird", () => {
   beforeEach(() => {
-    bird = new Bird("Rainbow-colored");
-  });
-
-  it("has a color property", () => {
-    expect(bird.color).to.equal("Rainbow-colored");
-  });
-});
+    bird = new Bird("Pablo", "male")
+    bird2 = new Bird("Mable", "female")
+  })
+  
+  it("has a class of Bird", () => {
+    expect(new Bird()).to.be.an.instanceof(Bird);
+  })
+  it("bird.speak() returns 'It's me! [name], the parrot!'", () => {
+    expect(bird.speak()).to.eq('It\'s me! Pablo, the parrot!')
+  })
+  it("bird2.speak() returns 'Squawk!'", () => {
+    expect(bird2.speak()).to.eq('Mable says squawk!')
+  })
+})
